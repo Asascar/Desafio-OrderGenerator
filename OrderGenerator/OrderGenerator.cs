1c4b2977f54f7cfaeae9c0fd749c202c7bd8ced4
Alexandre Borges <asascar2@gmail.com>
2023-04-28 10:38:26 -0300

    firth commit

---
+++ OrderGenerator/OrderGenerator.cs
@@ -0,0 +1,97 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+using System;
+
+namespace Executor
+{
+    public class OrderGenerator : QuickFix.MessageCracker, IApplication
+    {
+        private Session _session;
+        private int seqNum = 1;
+
+        public void OnCreate(SessionID sessionID)
+        {
+            _session = Session.LookupSession(sessionID);
+        }
+
+        public void OnLogon(SessionID sessionID)
+        {
+            Console.WriteLine("Logged in to session: " + sessionID);
+        }
+
+        public void FromApp(Message message, SessionID sessionID)
+        {
+            if (message is QuickFix.FIX42.ExecutionReport)
+            {
+                Console.WriteLine("Execution Reporter from OrderAccumulator" + message.ToString());
+
+            }
+            if (message is QuickFix.FIX42.OrderCancelReject)
+            {
+                Console.WriteLine("Cancel Reject from OrderAccumulator" + message.ToString());
+
+            }
+            Console.WriteLine("order: " + message.ToString());
+        }
+
+        public void ToApp(Message message, SessionID sessionID)
+        { }
+
+        public void OnLogout(SessionID sessionID)
+        {
+            Console.WriteLine("Logged out from session: " + sessionID);
+        }
+
+        public void OnMessage(QuickFix.FIX42.NewOrderSingle order, SessionID sessionID)
+        {
+            Console.WriteLine("Order Sent: OrderID=" + order.ClOrdID + " Symbol=" + order.Symbol.getValue() +
+            " Side=" + order.Side.getValue() + " Quantity=" + order.OrderQty.getValue() + " Price=" + order.Price.getValue());
+
+            Session.SendToTarget(order, _session.SessionID);
+        }
+        public void Start()
+        {
+            var random = new Random();
+            var symbols = new[] { "PETR4", "VALE3", "VIIA4" };
+            var sides = new[] { Side.BUY, Side.SELL };
+            var timer = new System.Timers.Timer(1000);
+            timer.Elapsed += (sender, e) =>
+            {
+                var symbol = symbols[random.Next(symbols.Length)];
+                var side = sides[random.Next(sides.Length)];
+                var quantity = random.Next(100000);
+                var price = Math.Round((decimal)(random.NextDouble() * 1000), 2);
+
+                var order = new QuickFix.FIX42.NewOrderSingle(
+                    new ClOrdID(Guid.NewGuid().ToString()),
+                    new HandlInst('1'),
+                    new Symbol(symbol),
+                    new Side(side),
+                    new TransactTime(DateTime.Now),
+                    new OrdType(OrdType.LIMIT)
+                );
+                order.Set(new OrderQty(quantity));
+                order.Set(new Price(price));
+                order.Header.SetField(new QuickFix.Fields.MsgSeqNum(seqNum++));
+
+                var mySessionID = new SessionID("FIX.4.2", "OrderGenerator", "OrderAccumulator");
+                Session.SendToTarget(order, mySessionID);
+
+            };
+            timer.Start();
+        }
+
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            Console.WriteLine("Called Admin :" + message.ToString());
+        }
+
+        public void FromAdmin(Message message, SessionID sessionID)
+        {
+
+            Console.WriteLine("response from AdmClient     " + message.ToString());
+
+        }
+
+    }
+}
---
+++ OrderGenerator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using QuickFix;
+using Acceptor;
+using QuickFix.Transport;
+
+namespace Executor
+{
+    class Program
+    {
+        private const string HttpServerPrefix = "http://127.0.0.1:5080/";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=============");
+            Console.WriteLine("Desafio ATG Alexandre Borges.");
+            Console.WriteLine();
+            Console.WriteLine("                                                    ! ! !");
+            Console.WriteLine("              Orquestrador de pedidos onde gero uma ordem por segundo e envio para o OrderAccumulator");
+            Console.WriteLine("                                                    ! ! !");
+            Console.WriteLine();
+            Console.WriteLine("=============");
+
+            try
+            {
+                OrderGenerator app = new OrderGenerator();
+                QuickFix.SessionSettings settings = new SessionSettings(@"simpleacc.cfg");
+                IApplication application = new OrderGenerator();
+                QuickFix.IMessageStoreFactory storeFactory = new QuickFix.FileStoreFactory(settings);
+                QuickFix.ILogFactory logFactory = new QuickFix.ScreenLogFactory(settings);
+                DefaultMessageFactory messageFactory = new DefaultMessageFactory();
+                SocketInitiator initiator = new SocketInitiator(application, storeFactory, settings, logFactory, messageFactory);
+
+                initiator.Start();
+                app.Start();
+                Console.WriteLine("press <enter> to quit");
+                Console.Read();
+                initiator.Stop();
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
